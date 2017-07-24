11a9f1de3b379e17b5013a8fed7fcc2760321f14
frazei <francesco.zei@gmail.com>
2017-07-24 19:30:10 +0200

    ottimizzazione caricamento immagini

--- slides.js
+++ slides.js
@@ -1,22 +1,49 @@
-var slides = [
-  "http://loremflickr.com/800/480/iceland?random=1",
-  "http://loremflickr.com/800/480/iceland?random=2",
-  "http://loremflickr.com/800/480/iceland?random=3",
-  "http://loremflickr.com/800/480/iceland?random=4",
-  "http://loremflickr.com/800/480/iceland?random=5"
-];
 var current = 0,
-  timer;
+  timer,
+  slides;
 
-function slideshow() {
-  var next = (current+1 >= slides.length) ? 0 : current+1;
-  console.log("current="+current+" | next="+next);
-  if(next == 0) $('div#slide_'+next).css('opacity', 1);
-  else {
-    $('div#slide_'+next).removeClass('transition').css('opacity', 1);
-    $('div#slide_'+current).addClass('transition').css('opacity', 0);
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
   }
-  current = next;
+  return array;
+}
+function loadSlides() {
+  var prev = (current-1 < 0) ? slides.length-1 : current-1;
+  var pprev = (current-2 < 0) ? slides.length-1 : current-2;
+  var next = (current+1 >= slides.length) ? 0 : current+1;
+  var nnext = (current+2 >= slides.length) ? 0 : current+2;
+
+  $('div#slide_'+nnext+', div#slide_'+pprev).css('background-image','');
+  $('nav#slides-pagination').html((current+1)+"/"+slides.length);
+  $('div#slide_'+current).css('opacity', 1).css('background-image','url('+slides[current]+')');
+  $('div#slide_'+next).css('background-image','url('+slides[next]+')');
+  $('div#slide_'+prev).css('background-image','url('+slides[prev]+')');
+}
+function transitionend() {
+  timer = setTimeout(function() {
+    slideshow();
+  }, 5000);
+}
+function slideshow() {
+  if($('div#slides').hasClass('auto')) {
+    var next = (current+1 >= slides.length) ? 0 : current+1;
+    console.log("current="+current+" | next="+next);
+    if(next == 0) $('div#slide_'+next).css('opacity', 1);
+    else {
+      $('div#slide_'+next).removeClass('transition').css('opacity', 1);
+      $('div#slide_'+current).addClass('transition').css('opacity', 0);
+    }
+    current = next;
+    loadSlides();
+  } else return false;
 }
 function slideTo(next) {
   console.log('slideTo '+next);
@@ -26,51 +53,52 @@ function slideTo(next) {
   $('div#slide_'+next).css('opacity', 1);
   $('div#slide_'+current).css('opacity', 0);
   current = next;
+  loadSlides();
   timer = setTimeout(function() {
-    playAuto();
+    autoPlay();
   }, 10000);
 }
-function playAuto() {
+function autoPlay() {
+  console.log('autoPlay');
+  // questo è quello che lo fa girare in automatico
+  $('div#slides div').on('transitionend', transitionend);
   $('div#slides').addClass('auto');
-  timer = setTimeout(function() {
-    slideshow();
-  }, 1000);
+  slideshow();
 }
 function stopAuto() {
   clearTimeout(timer);
+  // questo è quello che lo fa girare in automatico
+  $('div#slides div').off('transitionend', transitionend);
   $('div#slides').removeClass('auto');
 }
 function showSlides() {
-  $('div#slides, nav#slides-navigation').css('display', 'block');
+  $('div#slides, nav#slides-navigation, nav#slides-pagination').css('display', 'block');
   timer = setTimeout(function() {
-    playAuto();
+    autoPlay();
   }, 10000);
 }
 function hideSlides() {
   stopAuto();
-  $('div#slides, nav#slides-navigation').css('display', 'none');
+  $('div#slides, nav#slides-navigation, nav#slides-pagination').css('display', 'none');
 }
 
 $(function() {
-  $(slides).each(function(i,v) {
-    $('div#slides').append("<div id='slide_"+i+"' class='slides'></div>");
-    $('div#slide_'+i).css('z-index', (i*-1)).css('background-image','url('+v+')');
-  });
   $('div#slides')
     .css('width', $(document).width())
     .css('height', $(document).height());
 
-  // la prima slide appare subito
-  $('div#slide_'+current).css('opacity', 1);
+  $.getJSON( "config.json", function( data ) {
+    slides = shuffleArray(data['slides']);
+    $(slides).each(function(i,v) {
+      $('div#slides').append("<div id='slide_"+i+"' class='slides'></div>");
+      $('div#slide_'+i).css('z-index', (i*-1));
+    });
 
-  // questo è quello che lo fa girare in automatico
-  $('div#slides div').on('transitionend webkitTransitionEnd', function() {
-    console.log('transition end');
-    if($('div#slides').hasClass('auto')) slideshow();
+    // si esce dal fullscreen cliccando su una foto
+    $('div.slides').click(hideSlides);
+
+    loadSlides();
   });
-
-  // si esce dal fullscreen cliccando su una foto
-  $('div.slides').click(hideSlides);
 
   // avanti e indietro manuale
   $('nav#slides-navigation span#next').click(function() {
@@ -82,27 +110,3 @@ $(function() {
     slideTo(prev);
   });
 });
-/*var myTimer,
-  screenSaverTime = 6000,
-  screenSaverActive = false;
-
-function resetTimer() {
-  clearTimeout(myTimer);
-  screenSaverActive = false;
-  myTimer = setTimeout(function() { startScreenSaver(); }, screenSaverTime);
-}
-function startScreenSaver() {
-  if(!screenSaverActive) {
-    screenSaverActive = true;
-  }
-}
-
-$(function() {
-  myTimer = setTimeout(function() { startScreenSaver(); }, screenSaverTime);
-
-  $(document).bind('click', function() {
-    resetTimer();
-  }).mousemove(function() {
-    resetTimer();
-  });
-});*/
